ef81868cf1a10e554b15927f8f52a422af8ed477
tiffaw8 <tiffaw3@uci.edu>
2015-02-13 21:28:25 -0800

    createtable_bookstore
    SQL to create tables: user, contribution, adminstrator

---
+++ createtable_bookstore.sql
@@ -0,0 +1,34 @@
+CREATE TABLE user
+(
+	id int NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    num_contributions int,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE contribution
+(
+	id int NOT NULL,
+    user_id int NOT NULL,
+    contribution_name VARCHAR(255) NOT NULL,
+    isbn_num int NOT NULL,
+    book_rating int,
+    book_price decimal,
+    PRIMARY KEY(id),
+    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
+);
+
+CREATE TABLE adminstrator
+(
+	id int NOT NULL,
+	first_name VARCHAR(255),
+    last_name VARCHAR(255),
+	email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    num_contr_appr int,
+    num_user_appr int,
+    PRIMARY KEY(id)
+);
