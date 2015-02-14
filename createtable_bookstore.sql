6d5b78f268f60e70acf7a81ab8bc61613711461d
Jasper <choujs@uci.edu>
2015-02-14 13:19:21 -0800

    Update createtable_bookstore.sql

--- createtable_bookstore.sql
+++ createtable_bookstore.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user
+CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(255),
@@ -9,33 +9,33 @@ CREATE TABLE user
     PRIMARY KEY(id)
 );
 
-CREATE TABLE contribution
+CREATE TABLE contributions
 (
 	id int NOT NULL AUTO_INCREMENT,
     user_id int NOT NULL,
     contribution_name VARCHAR(255) NOT NULL,
     isbn_num int NOT NULL,
     book_rating int,
-    book_price decimal,
+    book_price double,
     description text,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE pending_contribution
+CREATE TABLE pending_contributions
 (
 	id int NOT NULL AUTO_INCREMENT,
     user_id int NOT NULL,
     contribution_name VARCHAR(255) NOT NULL,
     isbn_num int NOT NULL,
     book_rating int,
-    book_price decimal,
+    book_price double,
 	description text,
     PRIMARY KEY(id),
-    FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE adminstrator
+CREATE TABLE adminstrators
 (
 	id int NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(255),
