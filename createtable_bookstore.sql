c368fb9970e18dd8423a29c73614d7d730023eb0
Jasper <choujs@uci.edu>
2015-03-08 19:10:57 -0700

    Update createtable_bookstore.sql

--- createtable_bookstore.sql
+++ createtable_bookstore.sql
@@ -132,6 +132,26 @@ BEGIN
 
 END $$
 
+CREATE PROCEDURE submit_and_approve(
+_user_id INT,
+_contribution_name VARCHAR(255),
+_isbn_num INT,
+_book_price DOUBLE,
+_description text,
+_photo_url VARCHAR(255),
+_publisher VARCHAR(255)
+)
+BEGIN
+
+	START TRANSACTION;
+
+	INSERT INTO contributions VALUES(DEFAULT, _user_id, _contribution_name, isbn_num, 0, _book_price, _description, now(), _photo_url, _publisher);
+
+	COMMIT;
+
+END $$
+
+
 CREATE PROCEDURE approve_review(
 _review_id INT
 )
